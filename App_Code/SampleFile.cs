38be6957e1c0449685d49eccec519a48088ba3d8
Dinesh Gouttam <dinesh.gouttam@metacube.com>
2019-07-12 13:14:03 +0530

    Test Commit

---
+++ App_Code/SampleFile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GitDemo.App_Code
+{
+    public class SampleFile
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+    }
+}
